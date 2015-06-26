84c48239e666301d075f410a79a7c9f7e592d4be
Thiago Andrade <thiagoh@gmail.com>
2015-06-26 19:40:40 -0300

    twelventh solution

--- program12.1.js
+++ program12.1.js
@@ -1,16 +1,14 @@
 
 var http = require('http');
-var bl = require('bl');
 var map = require('through2-map')
 var port = process.argv[2];
 
 var server = http.createServer(function (request, response) {
-	var b = new bl();
-	request.pipe(b));
-	
+
 	response.writeHead(200, 'success', {'Content-Type': 'text/plain'});
-	response.write(b.toString().toUpperCase());
-	response.end();
+	request.pipe(map(function(chunk) {
+		return chunk.toString().toUpperCase();
+	})).pipe(response);
 });
 
 server.listen(Number(port));
